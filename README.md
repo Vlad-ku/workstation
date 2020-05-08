8bfa8a7e551fc1c2d5339eab5cead32d7f33e047
vlad-ku <tmnt-95@yandex.ru>
2020-05-08 14:05:09 +0800

    -

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-# Docker рабочая станция на archlinux
+# Archlinux рабочая станция на vagrant
 
-![docker-badge](http://dockeri.co/image/vladv8/workstation)
+TODO
 
 ## Запуск
 
-	docker run --rm -it \
-        -e passv='password VNC' \
-        -e passr='password SSH root' \
-        -e passu='password SSH user' \
-        -p 5901:5901 \
-        -p 23:22 \
-        vladv8/workstation
-
+TODO
--- mystart.sh
+++ mystart.sh
@@ -1,56 +1,44 @@
 # --- BEGIN обновляемся ---
-# pacman -Sy --noconfirm archlinux-keyring && pacman -Syyu --noconfirm
-pacman -Syu
-pacman -S  --noconfirm git sudo
+sudo pacman -Syu
+sudo pacman -S --noconfirm git
 # --- END обновляемся ---
 
 
 # --- BEGIN установка yay ---
 # base-devel для fakeroot
-# pacman -S --noconfirm base-devel
-# т.к. под root makepkg не работает (без sudo тоже не работает)
-RUN \
-    cd ~                                            && \
-    git clone https://aur.archlinux.org/yay-git.git && \
-    cd yay-git                                      && \
-    makepkg -si --noconfirm                         && \
-    cd ~                                            && \
-    rm -rf yay-git
+sudo pacman -S --noconfirm base-devel
+cd ~            && git clone https://aur.archlinux.org/yay-git.git
+cd ~/yay-git    && makepkg -si --noconfirm
+cd ~            && rm -rf yay-git
 # --- END установка yay ---
 
 
 # --- BEGIN настройка дот файлов ---
-USER user
-RUN \
-    cd ~                                                && \
-    git clone https://github.com/Vlad-ku/home-conf.git  && \
-    mv ./home-conf/.git ./                              && \
-    rm -rf home-conf                                    && \
-    git reset --hard
-USER root
+cd ~
+git clone https://github.com/Vlad-ku/home-conf.git
+mv ~/home-conf/.git ~/
+rm -rf ~/home-conf
+git reset --hard
 # --- END настройка дот файлов ---
 
 
-# --- BEGIN ставим X-ы, ssh и шрифты ---
-RUN pacman -S --noconfirm xorg-xauth
-RUN pacman -S --noconfirm openssh
-RUN pacman -S --noconfirm ttf-dejavu
-# генерируем ключ сервера для SSH
-RUN ssh-keygen -t ecdsa -b 521 -N '' -f /etc/ssh/ssh_host_ecdsa_key
-# --- END ставим X-ы, ssh и шрифты ---
+# --- BEGIN ставим X-ы и шрифты ---
+sudo pacman -S --noconfirm xorg-xauth
+sudo pacman -S --noconfirm ttf-dejavu
+# --- END ставим X-ы и шрифты ---
 
 
 # --- BEGIN ставим необходимое ПО ---
-RUN pacman -S --noconfirm vim neovim
-RUN pacman -S --noconfirm tigervnc
-RUN pacman -S --noconfirm i3
+sudo pacman -S --noconfirm vim neovim
+sudo pacman -S --noconfirm tigervnc
+sudo pacman -S --noconfirm i3
 # для ответов на вопросы при настройке VNC
-RUN pacman -S --noconfirm expect
+sudo pacman -S --noconfirm expect
 # --- END ставим необходимое ПО ---
 
 
 # --- BEGIN остальное ПО ---
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     htop                    \
     tmux                    \
     ranger                  \
@@ -58,82 +46,68 @@ RUN pacman -S --noconfirm   \
     thunar                  \
     termite
 # рабочий стол
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     dmenu                   \
     i3blocks                \
     i3lock
 # перехват клавиш / эмуляция клавиатуры
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     xbindkeys               \
     xdotool
 # работа с архивами
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     zip                     \
     unzip
 # создание скриншота / работа с буфером
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     scrot                   \
     xclip
 # аналог net-tools
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     iproute2
 # другое крупное ПО
-RUN pacman -S --noconfirm   \
+sudo pacman -S --noconfirm  \
     chromium
 # --- END остальное ПО ---
 
+exit 0
 
 # --- BEGIN иконочный шрифт NERDtree и powerline для vim / ссылки nvim / emoji шрифт ---
-USER user
-RUN \
-    mkdir -p ~/.local/share/fonts   && \
-    cd ~/.local/share/fonts         && \
-    curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
-RUN \
-    mkdir ~/.vim                    && \
-    ln -s ~/.vim ~/.config/nvim     && \
-    ln -s ~/.vimrc ~/.vim/init.vim
-USER root
-RUN pacman -S --noconfirm noto-fonts-emoji
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf"
+mkdir ~/.vim
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.vim/init.vim
+pacman -S --noconfirm noto-fonts-emoji
 # --- END иконочный шрифт NERDtree и powerline для vim / ссылки nvim / emoji шрифт ---
 
 
-# --- BEGIN ставим систему инициализации ---
-USER user
-RUN yay -S --noconfirm sysvinit
-USER root
-# --- END ставим систему инициализации ---
-
-
 # --- BEGIN настройка VNC, i3, и паролей ---
-USER user
-RUN \
-    cd ~                                                                                    && \
-    echo '#!/usr/bin/expec'                                                > startvnc.sh    && \
-    echo 'spawn /usr/sbin/vncserver'                                      >> startvnc.sh    && \
-    echo 'expect "Password:"'                                             >> startvnc.sh    && \
-    echo 'send   "vivaldi8\r"'                                            >> startvnc.sh    && \
-    echo 'expect "Verify:"'                                               >> startvnc.sh    && \
-    echo 'send   "vivaldi8\r"'                                            >> startvnc.sh    && \
-    echo 'expect "Would you like to enter a view-only password (y/n)?"'   >> startvnc.sh    && \
-    echo 'send   "n\r"'                                                   >> startvnc.sh    && \
-    echo 'set timeout -1'                                                 >> startvnc.sh    && \
-    echo 'expect eof'                                                     >> startvnc.sh    && \
-    expect startvnc.sh                                                                      && \
-    rm -rf startvnc.sh                                                                      && \
-    vncserver -kill :1                                                                      && \
-    echo '#!/bin/sh'        > /home/user/.vnc/xstartup                                      && \
-    echo 'exec i3'         >> /home/user/.vnc/xstartup                                      && \
-    echo "root:vivaldi8" | sudo chpasswd                                                    && \
-    echo "user:vivaldi8" | sudo chpasswd
-USER root
+cd ~
+echo '#!/usr/bin/expec'                                                  > startvnc.sh
+echo 'spawn /usr/sbin/vncserver'                                        >> startvnc.sh
+echo 'expect "Password:"'                                               >> startvnc.sh
+echo 'send   "vivaldi8\r"'                                              >> startvnc.sh
+echo 'expect "Verify:"'                                                 >> startvnc.sh
+echo 'send   "vivaldi8\r"'                                              >> startvnc.sh
+echo 'expect "Would you like to enter a view-only password (y/n)?"'     >> startvnc.sh
+echo 'send   "n\r"'                                                     >> startvnc.sh
+echo 'set timeout -1'                                                   >> startvnc.sh
+echo 'expect eof'                                                       >> startvnc.sh
+expect startvnc.sh
+rm -rf startvnc.sh
+vncserver -kill :1
+echo '#!/bin/sh'        > /home/user/.vnc/xstartup
+echo 'exec i3'         >> /home/user/.vnc/xstartup
+echo "root:vivaldi8" | sudo chpasswd
+echo "user:vivaldi8" | sudo chpasswd
 # --- END настройка VNC, i3, и паролей ---
 
 
 # --- BEGIN настраиваем службы ---
-RUN \
-    echo "ab:123:Once:su user -c vncserver"     >> /etc/inittab     && \
-    echo "ac:123:Once:/usr/sbin/sshd -D"        >> /etc/inittab
+echo "ab:123:Once:su user -c vncserver"     >> /etc/inittab
+echo "ac:123:Once:/usr/sbin/sshd -D"        >> /etc/inittab
 # --- END настраиваем службы ---
 
 
@@ -141,10 +115,3 @@ RUN \
 # TODO домашнюю папку в том
 # TODO тема для thunar и остального
 # TODO прокинуть docker
-
-
-# USER user
-EXPOSE 22
-EXPOSE 5901
-
-ENTRYPOINT ["/usr/sbin/init", "1"]
